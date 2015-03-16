7fa34fc09ab254a533016d867ab2d54dcb9d4877
SummersBrian <summers.brian.cs@gmail.com>
2015-03-16 12:01:14 -0400

    setting up image plane
    setting up the image plane for the scene, see the new pdf in root of
    directory

--- ps3/main.cpp
+++ ps3/main.cpp
@@ -6,6 +6,7 @@ Brian Summers - 110656609
 
 #include <stdlib.h>
 #include <iostream>
+#include <cmath>
 
 #include "BMPImage.h"
 #include "scene.h"
@@ -35,6 +36,25 @@ int main (int argc, char** argv) {
 
 	//TODO
 	//raytrace the scene
+
+	//image plane setup
+	float pHeight = tan(theScene.getFieldOfViewY() / 2) * -2;
+	float pWidth = pHeight * (imageWidth / imageHeight);
+	Vector3 pImageCenter = Vector3(0, 0, -1);
+	Vector3 pLowerLeft = pImageCenter - theScene.getEyeRight() * (pWidth / 2) - theScene.getEyeUp() * (imageHeight / 2);
+
+	//for each pixel in the BMPImage
+	//starting world coordinate positions, top left of image is (xmin, ymax)
+	int xPos = -(imageWidth / 2);
+	for (int i = 0; i < imageWidth; i++) {
+		xPos += i;
+		int yPos = imageHeight / 2;
+		for (int j = 0; j < imageHeight; j++) {
+			yPos -= j;
+			//cast a ray from the focal point through the pixel
+			//Vector3;
+		}
+	}
 
 
 	if (theImage.save(argv[2]) != 1) {
